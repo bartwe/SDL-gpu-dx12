f0e8b63180085ed883591eab7a22ec2e9b64b757
Bart van der Werf <bluelive@gmail.com>
2024-08-11 02:04:07 +0200

    Limit api to 8bit stencils, which is a dx11 and dx12 limit
    https://github.com/thatcosmonaut/SDL/issues/146

--- include/SDL3/SDL_gpu.h
+++ include/SDL3/SDL_gpu.h
@@ -301,7 +301,7 @@ typedef enum SDL_GpuColorComponentFlagBits
     SDL_GPU_COLORCOMPONENT_A_BIT = 0x00000008
 } SDL_GpuColorComponentFlagBits;
 
-typedef Uint32 SDL_GpuColorComponentFlags;
+typedef Uint8 SDL_GpuColorComponentFlags;
 
 typedef enum SDL_GpuFilter
 {
@@ -378,7 +378,7 @@ typedef enum SDL_GpuDriver
 typedef struct SDL_GpuDepthStencilValue
 {
     float depth;
-    Uint32 stencil;
+    Uint8 stencil;
 } SDL_GpuDepthStencilValue;
 
 typedef struct SDL_GpuViewport
@@ -584,9 +584,9 @@ typedef struct SDL_GpuDepthStencilState
     SDL_bool stencilTestEnable;
     SDL_GpuStencilOpState backStencilState;
     SDL_GpuStencilOpState frontStencilState;
-    Uint32 compareMask;
-    Uint32 writeMask;
-    Uint32 reference;
+    Uint8 compareMask;
+    Uint8 writeMask;
+    Uint8 reference;
 } SDL_GpuDepthStencilState;
 
 typedef struct SDL_GpuColorAttachmentDescription
--- src/gpu/d3d11/SDL_gpu_d3d11.c
+++ src/gpu/d3d11/SDL_gpu_d3d11.c
@@ -452,7 +452,7 @@ typedef struct D3D11GraphicsPipeline
     Uint8 hasDepthStencilAttachment;
     DXGI_FORMAT depthStencilAttachmentFormat;
     ID3D11DepthStencilState *depthStencilState;
-    Uint32 stencilRef;
+    Uint8 stencilRef;
 
     SDL_GpuPrimitiveType primitiveType;
     ID3D11RasterizerState *rasterizerState;
@@ -3471,7 +3471,7 @@ static void D3D11_BeginRenderPass(
                 dsv,
                 dsClearFlags,
                 depthStencilAttachmentInfo->depthStencilClearValue.depth,
-                (Uint8)depthStencilAttachmentInfo->depthStencilClearValue.stencil);
+                depthStencilAttachmentInfo->depthStencilClearValue.stencil);
         }
     }
 
